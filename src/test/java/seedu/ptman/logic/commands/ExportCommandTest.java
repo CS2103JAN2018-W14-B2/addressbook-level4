a0b1e95a04068ff15a05543b51b0af7220720a50
hzxcaryn <heng.zhixuan.caryn@dhs.sg>
2018-03-27 16:11:12 +0800

    Add test for equals

--- src/test/java/seedu/ptman/logic/commands/ExportCommandTest.java
+++ src/test/java/seedu/ptman/logic/commands/ExportCommandTest.java
@@ -1,6 +1,7 @@
 package seedu.ptman.logic.commands;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static seedu.ptman.logic.commands.ExportCommand.MESSAGE_EMAIL_SUCCESS;
 import static seedu.ptman.logic.commands.ExportCommand.MESSAGE_SAVE_SUCCESS;
@@ -33,5 +34,29 @@ public class ExportCommandTest {
         assertTrue(eventsCollectorRule.eventsCollector.getMostRecent()
                 instanceof ExportTimetableAsImageAndEmailRequestEvent);
         assertTrue(eventsCollectorRule.eventsCollector.getSize() == 1);
+    }
+
+    @Test
+    public void equals() {
+        Email aliceEmail = new Email("alice@example.com");
+        Email bobEmail = new Email("bob@example.com");
+        ExportCommand exportAliceCommand = new ExportCommand(aliceEmail);
+        ExportCommand exportBobCommand = new ExportCommand(bobEmail);
+
+        // same object -> returns true
+        assertTrue(exportAliceCommand.equals(exportAliceCommand));
+
+        // same values -> returns true
+        ExportCommand exportAliceCommandCopy = new ExportCommand(aliceEmail);
+        assertTrue(exportAliceCommand.equals(exportAliceCommandCopy));
+
+        // different types -> returns false
+        assertFalse(exportAliceCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(exportAliceCommand.equals(null));
+
+        // different email -> returns false
+        assertFalse(exportAliceCommand.equals(exportBobCommand));
     }
 }
